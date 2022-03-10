fffdd21cfe047739fe084964c6a5d5a8647a6698
sujiaheng <648952316@qq.com>
2022-03-10 15:57:58 +0800

    refactor: vue example

--- with-vue/src/App.vue
+++ with-vue/src/App.vue
@@ -1,35 +1,46 @@
 <script setup lang="ts">
-import { ref, onMounted } from "vue";
-import { createFastboard, mount } from "@netless/fastboard";
+import { ref, onMounted, markRaw, onUnmounted, shallowRef } from "vue";
+import { createFastboard, FastboardApp, mount } from "@netless/fastboard";
 
-const container = ref(null);
+const container = ref<HTMLDivElement>();
 
-async function mountFastboard(div: HTMLDivElement) {
-  const app = await createFastboard({
-    // [1]
-    sdkConfig: {
-      appIdentifier: "whiteboard-appid",
-      region: "cn-hz", // "cn-hz" | "us-sv" | "sg" | "in-mum" | "gb-lon"
-    },
-    // [2]
-    joinRoom: {
-      uid: "unique_id_for_each_client",
-      uuid: "room-uuid",
-      roomToken:
-        "NETLESSROOM_...",
-    },
-    // [3]
-    managerConfig: {
-      cursor: true,
-    },
-  });
-  return mount(app, div, {});
-}
+// app not plain object, don't put it in ref().
+const appRef = shallowRef<FastboardApp | null>(null);
 
-onMounted(() => {
-  if (container.value) {
-    mountFastboard(container.value);
-  }
+onMounted(async () => {
+    let app = await createFastboard({
+        // [1]
+        sdkConfig: {
+            appIdentifier: "",
+            region: "cn-hz", // "cn-hz" | "us-sv" | "sg" | "in-mum" | "gb-lon"
+        },
+        // [2]
+        joinRoom: {
+            uid: "unique_id_for_each_client",
+            uuid: "room-uuid",
+            roomToken:
+                "NETLESSROOM_...",
+            },
+        // [3]
+        managerConfig: {
+            cursor: true,
+        },
+    });
+
+    appRef.value = app;
+
+    if (container.value) {
+        const ui = mount(app, container.value, {});
+        ui.update({ theme: "dark" });
+
+        app.insertCodeEditor(); // insert code editor to board;
+    }
+});
+
+onUnmounted(() => {
+    if (appRef.value) {
+        appRef.value.destroy();
+    }
 });
 </script>
 
